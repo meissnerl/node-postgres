c3320439d0bee5b6abb33d2e815a5528215b34f4
Logan Meissner <logan@loganmeissner.com>
2026-08-04 00:59:57 -0500

    chore(pg-pool): retry delay tuning

--- packages/pg-pool/retry.js
+++ packages/pg-pool/retry.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const DEFAULT_MAX_ATTEMPTS = 4
-const DEFAULT_BASE_DELAY_MS = 150
+const DEFAULT_BASE_DELAY_MS = 175
 
 /**
  * Acquire a client from the pool, retrying transient connection failures.
